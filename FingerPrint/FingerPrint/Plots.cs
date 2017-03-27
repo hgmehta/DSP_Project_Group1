5a1c4627e7048c81446ad752f67f1839426d660e
hgmehta <harshmehta871997.hm10@hotmail.com>
2017-03-27 09:31:28 +0530

    Fingerprint matching using SourceAFIS SDK

---
+++ FingerPrint/FingerPrint/Plots.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Graph = System.Windows.Forms.DataVisualization.Charting;
+
+
+namespace FingerPrint
+{
+    public partial class Plots : Form
+    {
+        Graph.Chart chart;
+        public Plots()
+        {
+            InitializeComponent();
+            this.ClientSize = new System.Drawing.Size(750, 900);
+        }
+
+        private void Plots_Load(object sender, EventArgs e)
+        {
+            const int MaxX = 1;
+            // Create new Graph
+            chart = new Graph.Chart();
+            chart.Location = new System.Drawing.Point(10, 10);
+            chart.Size = new System.Drawing.Size(700, 700);
+            // Add a chartarea called "draw", add axes to it and color the area black
+            chart.ChartAreas.Add("draw");
+            chart.ChartAreas["draw"].AxisX.Minimum = 0;
+            chart.ChartAreas["draw"].AxisX.Maximum = MaxX;
+            chart.ChartAreas["draw"].AxisX.Interval = 0.1;
+            chart.ChartAreas["draw"].AxisX.MajorGrid.LineColor = Color.White;
+            chart.ChartAreas["draw"].AxisX.MajorGrid.LineDashStyle = Graph.ChartDashStyle.Dash;
+            chart.ChartAreas["draw"].AxisY.Minimum = 0;
+            chart.ChartAreas["draw"].AxisY.Maximum = 1;
+            chart.ChartAreas["draw"].AxisY.Interval = 0.1;
+            chart.ChartAreas["draw"].AxisY.MajorGrid.LineColor = Color.White;
+            chart.ChartAreas["draw"].AxisY.MajorGrid.LineDashStyle = Graph.ChartDashStyle.Dash;
+
+            chart.ChartAreas["draw"].BackColor = Color.White;
+
+            // Create a new function series
+            chart.Series.Add("FAR");
+            chart.Series.Add("FRR");
+            // Set the type to line      
+            chart.Series["FAR"].ChartType = Graph.SeriesChartType.Line;
+            chart.Series["FRR"].ChartType = Graph.SeriesChartType.Line;
+            // Color the line of the graph light green and give it a thickness of 3
+            chart.Series["FAR"].Color = Color.LightGreen;
+            chart.Series["FAR"].BorderWidth = 3;
+            chart.Series["FRR"].Color = Color.Blue;
+            chart.Series["FRR"].BorderWidth = 3;
+            //This function cannot include zero, and we walk through it in steps of 0.1 to add coordinates to our series
+
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\harsh\Documents\Visual Studio 2015\Projects\FingerPrint\FingerPrint\normalizedscore.txt");
+            float[] score = new float[lines.Length];
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                score[i] = float.Parse(lines[i]);
+            }
+            float[] far = new float[100];
+            float[] frr = new float[100];
+
+            float threshold = 0;
+            int k = 0;
+            for (threshold = 0; threshold<=1; threshold += (float)0.1)
+            {
+                far[k] = 0;
+                for (int i = 0; i < score.Length; i++)
+                { 
+                    if (score[i] > threshold)
+                    {
+                        far[k]++;
+                    }
+                }
+                far[k] = far[k] / 300;
+
+                frr[k] = 0;
+                for (int j = 0; j < score.Length; j += 11)
+                {
+                    if (score[j] < threshold)
+                    {
+                        frr[k]++;
+                    }
+                }
+
+                frr[k] = frr[k] / 30;
+                k++;
+            }
+            k = 0;
+            for (double x = 0; x <= MaxX; x += 0.1)
+            {
+
+                chart.Series["FAR"].Points.AddXY(x, far[k]);
+                chart.Series["FRR"].Points.AddXY(x, 1-far[k]);
+                k++;
+            }
+            chart.Series["FAR"].LegendText = "FAR";
+            chart.Series["FRR"].LegendText = "FRR";
+            // Create a new legend called "MyLegend".
+            chart.Legends.Add("FAR");
+            chart.Legends.Add("FRR");
+            chart.Legends["FAR"].BorderColor = Color.Tomato; // I like tomato juice!
+            chart.Legends["FRR"].BorderColor = Color.Tomato; // I like tomato juice!
+            Controls.Add(this.chart);
+        }
+    }
+}
---
+++ FingerPrint/FingerPrint/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SourceAFIS.Simple;
+using System.Windows.Media.Imaging;
+using System.IO;
+using System.Windows.Forms;
+using System.Linq;
+
+namespace FingerPrint
+{
+    class Program
+    {
+        static void buildDB()
+        {
+            AfisEngine Afis = new AfisEngine();
+
+            Afis.Threshold = 5;
+
+            Fingerprint fp1 = new Fingerprint();
+            Fingerprint fp2 = new Fingerprint();
+
+            string path = @"C:\Users\harsh\Documents\Visual Studio 2015\Projects\FingerPrint\FingerPrint\score.txt";
+
+            string[] files = new string[100];
+
+            int k = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    files[k] = @"C:\Users\harsh\Documents\Visual Studio 2015\Projects\FingerPrint\FingerPrint\Database\" + (i + 1).ToString() + "_" + j.ToString() + ".bmp";
+                    k++;
+                }
+            }
+
+            k = 0;
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                fp1.AsBitmapSource = new BitmapImage(new Uri(files[i], UriKind.RelativeOrAbsolute));
+                Person person1 = new Person();
+                person1.Fingerprints.Add(fp1);
+                Afis.Extract(person1);
+
+                for (int j = 0; j < files.Length; j++)
+                {
+                    fp2.AsBitmapSource = new BitmapImage(new Uri(files[j], UriKind.RelativeOrAbsolute));
+                    Person person2 = new Person();
+                    person2.Fingerprints.Add(fp2);
+
+                    Afis.Extract(person2);
+
+                    float score = Afis.Verify(person1, person2);
+                    bool match = (score > Afis.Threshold);
+                    Console.WriteLine(files[i] + "\n Compare with " + files[j]);
+                    Console.WriteLine(score.ToString());
+                    using (StreamWriter sw = File.AppendText(path))
+                    {
+                        sw.WriteLine(score.ToString());
+                    }
+                    k++;
+                }
+                Console.WriteLine("\n");
+            }
+        }
+
+        static void normalizeScore()
+        {
+
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\harsh\Documents\Visual Studio 2015\Projects\FingerPrint\FingerPrint\score.txt");
+            float[] score = new float[lines.Length];
+            
+            for (int i = 0; i < lines.Length; i++)
+            {
+                score[i] = float.Parse(lines[i]);
+            }
+
+            //Console.WriteLine(max);
+
+            float min = score.Min();
+            float max = score.Max();
+
+            for (int i = 0; i < score.Length; i++)
+            {
+                score[i] = (score[i] - min) / (max - min);
+            }
+
+
+            string Path = @"C:\Users\harsh\Documents\Visual Studio 2015\Projects\FingerPrint\FingerPrint\normalizedscore.txt";
+
+
+            using (StreamWriter sw = File.AppendText(Path))
+            {
+                for (int i = 0; i < score.Length; i++)
+                { 
+                    sw.WriteLine(score[i].ToString());
+                }
+            }
+          //  string s = Console.ReadLine();
+        }
+
+        static void test()
+        {
+            AfisEngine Afis = new AfisEngine();
+
+            Afis.Threshold = (float)100.12;
+            Console.WriteLine(Afis.Threshold.ToString());
+            string files1 = @"C:\Users\harsh\Documents\Visual Studio 2015\Projects\FingerPrint\FingerPrint\Database\01_1.bmp";
+            string files2 = @"C:\Users\harsh\Documents\Visual Studio 2015\Projects\FingerPrint\FingerPrint\Database\01_2.bmp";
+
+            Fingerprint fp1 = new Fingerprint();
+            fp1.AsBitmapSource = new BitmapImage(new Uri(files1, UriKind.RelativeOrAbsolute));
+
+            Fingerprint fp2 = new Fingerprint();
+            fp2.AsBitmapSource = new BitmapImage(new Uri(files2, UriKind.RelativeOrAbsolute));
+
+            Person person1 = new Person();
+            person1.Fingerprints.Add(fp1);
+
+            Person person2 = new Person();
+            person2.Fingerprints.Add(fp2);
+
+
+            Afis.Extract(person1);
+            Afis.Extract(person2);
+            float score = Afis.Verify(person1, person1);
+            bool match = (score > Afis.Threshold);
+            Console.WriteLine(score.ToString());
+
+            string temp = Console.ReadLine();
+        }
+
+        static void Main(string[] args)
+        {
+            buildDB();
+            //test();
+            //normalizeScore();
+            //Application.EnableVisualStyles();
+            //Application.Run(new Plots());
+            
+        }
+    }
+}
